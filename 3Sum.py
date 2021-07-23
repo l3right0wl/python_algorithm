c81dfc97a9889669db2e0e5ed96c3a185ad98249
Taeun <85931489+l3right0wl@users.noreply.github.com>
2021-07-24 00:17:32 +0900

    07-09 3Sum

---
+++ 3Sum.py
@@ -0,0 +1,64 @@
+# 07 - 09
+# 배열을 입력받아 합으로 0을 만들 수 있는 3개의 엘리먼트를 출력하라.
+
+# 브루트 포스로 계산
+class Solution1:
+    def three_sum(self, nums: list[int]) -> list[list[int]]:
+        results = []
+        nums.sort()
+
+        for i in range(len(nums)-2):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            for j in range(i+1, len(nums)-1):
+                if j > i+1 and nums[j] == nums[j-1]:
+                    continue
+                for k in range(j+1, len(nums)):
+                    if k > j+1 and nums[k] == nums[k-1]:
+                        continue
+                    if nums[i] + nums[j] + nums[k] == 0:
+                        results.append([nums[i], nums[j], nums[k]])
+
+        return results
+
+
+sol1 = Solution1()
+nums = [-1, 0, 1, 2, -1, -4]
+print(sol1.three_sum(nums))
+
+
+# 투 포인터로 합 계산
+class Solution2:
+    def three_sum(self, nums: list[int]) -> list[list[int]]:
+        results = []
+        nums.sort()
+
+        for i in range(len(nums) - 2):
+            # 중복된 값 건너뛰기
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+
+            # 간격을 좁혀가며 합 sum 계산
+            left, right = i+1, len(nums) - 1
+            while left < right:
+                sum = nums[i] + nums[left] + nums[right]
+                if sum > 0:
+                    right -= 1
+                elif sum < 0:
+                    left += 1
+                else:
+                    # sum = 0인 경우이므로 정답 및 스킵처리
+                    results.append([nums[i], nums[left], nums[right]])
+
+                    while left < right and nums[left] == nums[left+1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right-1]:
+                        right -= 1
+                    right -= 1
+                    left += 1
+
+        return results
+
+sol2 = Solution2()
+nums = [-1, 0, 1, 2, -1, -4]
+print(sol2.three_sum(nums))
