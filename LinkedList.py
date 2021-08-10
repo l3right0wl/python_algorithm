88bdd4751d9cfa1addb8fbceac07e0a3340cc730
Taeun <85931489+l3right0wl@users.noreply.github.com>
2021-08-10 18:17:59 +0900

    LinkedList

--- LinkedList.py
+++ LinkedList.py
@@ -1,20 +1,21 @@
-class Node():
-  def __init__(self, data):
-    self.data = data
-    self.next = None
-    
-class LinkedList:
-  def __init__(self):
-    self.head = None
-    self.nodeCount = 0
-    
-  def append(self, node):
-    if self.head == None:
-      self.head = node
-    else:
-      cur = self.head
-      while cur.next != None:
-        cur = cur.next
-      cur.next = node
-     
-    
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+        
+class ListNode:
+    def __init__(self):
+        self.head = None
+        self.count = 0
+
+    # 추가 (마지막 값)
+    def add(self, node):
+        if self.head is None:
+            self.head = node
+        else:
+            cur = self.head
+            while cur.next is not None:
+                cur = cur.next
+            cur.next = node
+        self.count += 1
