dcb6ff540918af734db213bb4a40e8599cc1123f
Taeun <85931489+l3right0wl@users.noreply.github.com>
2021-08-19 00:00:06 +0900

    Update DailyTemperatures.py

--- DailyTemperatures.py
+++ DailyTemperatures.py
@@ -1,6 +1,7 @@
 # 09 - 22
 # 매일의 화씨 온도 리스트를 T를 입력받아서, 더 따듯한 날씨를 위해서는 며칠을 더 기다려야 하는지를 출력하라
 
+# 스택 값 비교
 class Solution1:
     def daily_temperatures(self, T: list[int]) -> list[int]:
         answer = [0] * len(T)
